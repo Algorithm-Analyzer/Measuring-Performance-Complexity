2d90b64d250a51ef1b93b264f53c998548ea9c17
22UG3-0651 <22ug3-0651@sltc.ac.lk>
2025-11-12 13:22:38 +0530

    SortingComparisonTool

--- SortingComparisonTool.java
+++ SortingComparisonTool.java
@@ -1,3 +1,10 @@
 import java.util.*;
 
 public class SortingComparisonTool {
+
+    // BUBBLE SORT - Simple but slow
+    // Like sorting cards by repeatedly swapping adjacent ones that are in wrong order
+    public static int bubbleSort(int[] arr) {
+        int n = arr.length; // Get how many numbers we have
+        int steps = 0; // Count how many comparisons we make
+        
