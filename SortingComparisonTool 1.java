f9b26685e609ca692d8c936b570ebe36190396c3
22UG3-0651 <22ug3-0651@sltc.ac.lk>
2025-11-13 12:32:55 +0530

    Average time

--- SortingComparisonTool 1.java
+++ SortingComparisonTool 1.java
@@ -40,12 +40,14 @@ public class AlgorithmPerformance {
             }
 
             long endTime = System.nanoTime();
-
+            
             // Average time per search
             double timeMs = (endTime - startTime) / 1_000_000.0 / iterations;
             printRow(size, timeMs);
         }
     }
+    
+
 
 
  
